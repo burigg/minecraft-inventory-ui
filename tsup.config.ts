b574613a94f659cb19efd86e402bdd8b033cb316
kabaodao <kabaodao@gmail.com>
2025-12-13 22:26:27 +0900

    chore: delete tsup and add tsdown

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts"],
-  dts: true,
-  minify: true,
-  clean: true,
-  external: ["react", "react-dom"],
-});
