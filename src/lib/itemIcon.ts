46d9bf1ac1f620ff266edf9b09b5991b176bf0ca
kabaodao <kabaodao@gmail.com>
2025-12-14 00:11:02 +0900

    chore: update package configuration

--- src/lib/itemIcon.ts
+++ src/lib/itemIcon.ts
@@ -1,46 +1,64 @@
-type ItemId = "GOLDEN_APPLE"
-| "COOKED_BEEF"
-| "BOW"
-| "BUCKET"
-| "LAVA_BUCKET"
-| "WATER_BUCKET"
-| "DIAMOND_AXE"
-| "DIAMOND_PICKAXE"
-| "DIAMOND_SHOVEL"
-| "DIAMOND_SWORD"
-| "FISHING_ROD"
-| "COBBLESTONE"
-| "WOOD"
-| "STONE_AXE"
-| "STONE_PICKAXE"
-| "STONE_SHOVEL"
-| "STONE_SWORD"
+import appleGolden from "../images/items/apple_golden.png";
+import beefCooked from "../images/items/beef_cooked.png";
+import bow from "../images/items/bow.png";
+import bucketEmpty from "../images/items/bucket_empty.png";
+import bucketLava from "../images/items/bucket_lava.png";
+import bucketWater from "../images/items/bucket_water.png";
+import diamondAxe from "../images/items/diamond_axe.png";
+import diamondPickaxe from "../images/items/diamond_pickaxe.png";
+import diamondShovel from "../images/items/diamond_shovel.png";
+import diamondSword from "../images/items/diamond_sword.png";
+import enderPearl from "../images/items/ender_pearl.png";
+import fishingRod from "../images/items/fishing_rod.png";
+import invCobblestone from "../images/items/inv_cobblestone.png";
+import invOakPlank from "../images/items/inv_oak_plank.png";
+import stoneAxe from "../images/items/stone_axe.png";
+import stonePickaxe from "../images/items/stone_pickaxe.png";
+import stoneShovel from "../images/items/stone_shovel.png";
+import stoneSword from "../images/items/stone_sword.png";
 
-const icons = import.meta.glob("../images/items/*.png", { eager: true , as: "url"}) as Record<string, string>;
+type ItemId =
+  | "GOLDEN_APPLE"
+  | "COOKED_BEEF"
+  | "BOW"
+  | "BUCKET"
+  | "LAVA_BUCKET"
+  | "WATER_BUCKET"
+  | "DIAMOND_AXE"
+  | "DIAMOND_PICKAXE"
+  | "DIAMOND_SHOVEL"
+  | "DIAMOND_SWORD"
+  | "ENDER_PEARL"
+  | "FISHING_ROD"
+  | "COBBLESTONE"
+  | "WOOD"
+  | "STONE_AXE"
+  | "STONE_PICKAXE"
+  | "STONE_SHOVEL"
+  | "STONE_SWORD";
 
-const ITEM_FILE: Record<ItemId, string> = {
-  GOLDEN_APPLE: "apple_golden.png",
-  COOKED_BEEF: "beef_cooked.png",
-  BOW: "bow.png",
-  BUCKET: "bucket_empty.png",
-  LAVA_BUCKET: "bucket_lava.png",
-  WATER_BUCKET: "bucket_water.png",
-  DIAMOND_AXE: "diamond_axe.png",
-  DIAMOND_PICKAXE: "diamond_pickaxe.png",
-  DIAMOND_SHOVEL: "diamond_shovel.png",
-  DIAMOND_SWORD: "diamond_sword.png",
-  FISHING_ROD: "fishing_rod.png",
-  COBBLESTONE: "inv_cobblestone.png",
-  WOOD: "inv_oak_plank.png",
-  STONE_AXE: "stone_axe.png",
-  STONE_PICKAXE: "stone_pickaxe.png",
-  STONE_SHOVEL: "stone_shovel.png",
-  STONE_SWORD: "stone_sword.png",
+const ITEM_ICONS: Record<ItemId, string> = {
+  GOLDEN_APPLE: appleGolden,
+  COOKED_BEEF: beefCooked,
+  BOW: bow,
+  BUCKET: bucketEmpty,
+  LAVA_BUCKET: bucketLava,
+  WATER_BUCKET: bucketWater,
+  DIAMOND_AXE: diamondAxe,
+  DIAMOND_PICKAXE: diamondPickaxe,
+  DIAMOND_SHOVEL: diamondShovel,
+  DIAMOND_SWORD: diamondSword,
+  ENDER_PEARL: enderPearl,
+  FISHING_ROD: fishingRod,
+  COBBLESTONE: invCobblestone,
+  WOOD: invOakPlank,
+  STONE_AXE: stoneAxe,
+  STONE_PICKAXE: stonePickaxe,
+  STONE_SHOVEL: stoneShovel,
+  STONE_SWORD: stoneSword,
 };
 
 export function getItemIconUrl(id: string): string | undefined {
   const itemId = id as ItemId;
-  const fileName = ITEM_FILE[itemId];
-  if (!fileName) return undefined;
-  return icons[`../images/items/${fileName}`];
+  return ITEM_ICONS[itemId];
 }
